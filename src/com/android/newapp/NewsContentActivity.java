6cb5eecf73f5ff5120cf97b804c3acc582fb9b9c
evitazhang <evitazhang@walltechsystem.cn>
2014-11-06 23:44:27 +0800

    布局错误

--- src/com/android/newapp/NewsContentActivity.java
+++ src/com/android/newapp/NewsContentActivity.java
@@ -23,7 +23,7 @@ public class NewsContentActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.news_content_layout);
 		String title = getIntent().getStringExtra("title");
 		String content = getIntent().getStringExtra("content");
 		NewsContentFragment nc = (NewsContentFragment) getFragmentManager()
